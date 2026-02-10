7e6dd306379bf4b220192a4e8456439d5e38b15e
Punnoose Punnen <punnoose.punnen@goniyo.com>
2026-02-10 22:16:08 +0530

    updated message

--- src/data/envelopes.ts
+++ src/data/envelopes.ts
@@ -3,7 +3,7 @@ import type { Envelope } from '../types';
 export const envelopes: Envelope[] = [
   {
     day: 1,
-    title: "Grateful day",
+    title: "Grateful",
     content: `So, as you know its the first time ive had a girlfriend for valentines day and since you just accepted to be my valentine😋 I wanted to go all out and make something for you. Celebrating valentines day while being this far away is PAINFULLL, butttt Ill be seeing you next weekend😋. So ive been brainstorming and came up with a plan. Other people have rose day chocolate day etc, but we different bro. We make our own days. At least thats the plan. So. Today is “Grateful day”.
     Im so so grateful you are in my life. You came to my life when I least expected it. And neither of us imagined we would be here right now when we matched on hinge almost 7 months ago. From that to now. We have come a LONG way. And im so grateful for it. Everyday I wake up happy knowing you are there for me. And that you are in my life. You are such an amazing person and im lucky to have even met you. Im grateful for everything. Every single gift(Each and every one of them are so special. Even the tissues). All the cars you bought me(best girlfriend ever). The songs you send. The random tea you drop. How you make me feel seen and heard. You make me want to do better. Thank you for being you. 
     Im SO grateful for every wrong turn that led me to you.
@@ -13,7 +13,7 @@ I love you and im so happy you are in my life🩷`
   },
   {
     day: 2,
-    title: "What I love about you day ",
+    title: "What I love about you",
     content: `Ah. Theres just so many things that i cant even list down. But ill try mentioning a few. I love how you have your opinions on things and you stick to it regardless of who says what. Im so proud of you for it and i respect you for it. I love how strong you are. Well things are not exactly smooth at home. Uve had some bad experiences but you fought through all of them. No one has to go through all this. But you are handling it so well and im so proud of you for it. I love how you are ready to listen. Sometimes im just spinning out and im stuck in my head and you are ready to listen. We have fights. And you were ready to hear me out even when i messed up. I love how hard working and genuine you are. If you do something you actually put your heart and soul into it and its such a respectable quality. I love how you appreciate the little things. And i love ur taste in fashion and music. Its so good and im glad a bit of it is rubbing off on me. I love everything about you. The person you are. I love you on your bad days.
       Again. There are so many more thingsss. 
 
@@ -22,20 +22,25 @@ I love you on your good days and i love you even more on your bad days. And im n
   },
   {
     day: 3,
-    title: "A Memory Day",
+    title: "The memories",
     content: `There are so many moments that i keep thinking of. Most are funny and make me laugh. Some make me miss you. Some make me feel loved. So i just wanted to pick some of my favourite memories. Our first date is obviously on the list. I was SO nervous. New place. New people. Just to meet a girl I’ve never met before. Someone i cared about deeply. I remember waiting in front of your hostel with a bunch of Lillies in my hand. Sweating SO MUCH. And then i saw you walking down. In bibtya. And man. Uff. Gone. We went to have lunch and i couldn’t stop staring at you. We have been dating for almost 7 months but every single time i see you i am just so amazed. I just want to sit and stare. Last time we met. And we were sleeping. It just felt like home. It just felt right. I can’t explain how good it felt to sleep next to you. Babee. Literally every single memory is so special. Every second with you is so special. That time i asked you to be my girlfriend. Standing outside of polar bear in the midfield of the night. Asking you to be my girlfriend even though we hadn’t even  met each other. God there are so many good memories and i am so glad you are in my life. The thought of you whistling at a rat keeps popping up in my head randomly ahhh its one of my favourite memories in life. So many precious moments. Me buying momos for you. Crying together in airport. Staying up till 4 am doomscrolling and then regretting it the next day but only to do it again. 
     
   I love you and i can’t wait to make more and more memories together. 🩷`
   },
   {
     day: 4,
-    title: "",
-    content: ``
+    title: "How you make me feel",
+    content: `I keep saying this or maybe i don’t say this enough. You have a really big impact on me. My mood gets directly affected by our conversations. And you have become a huge part of my life. And im happy about it. I am not perfect. And i am just trying to be the best boyfriend i can so that i might come close to being the guy you deserve. But i mess up at times and you forgive me for it. You dont hold it against me. You genuinely give me another chance and it means a LOT. Everything you do seems cute to me. LITTERALLY EVERYTHINGG. Lowkey concerning. Sometimes im like wdymm my baby is sleepingg awww. And just thinking about you put a smile on my face. Most days you are the first and last person i talk to during the day and it makes me so happy. I think about you and i am smiling. I look around my room its covered in things that remind me of you. I go to work and my desk is covered with the things you bought. Im happy. Im happy in life.
+Sometimes i get stuck in my head. I start assuming the worst and make up scenarios in my head. Idk how to control it but you do help me with it. I can talk to you about anything. Being comfortable with someone else is such a blessing and i feel i have been blessed. 
+
+I love you and thank you for making me this comfortable🩷`
   },
   {
     day: 5,
-    title: "",
-    content: ``
+    title: "Missing you",
+    content: `Well this needs no introduction. Sometimes i crave your presence more than i want to breathe. Sometimes seeing other couples makes me want to see you immediately. Being disconnected from you puts me in a weird place. Our entire relationship has been long distance. We started off in a pretty unusual way. Yes its tough. Yes there are days where i am reallyyyy down. Just wanting a hug so bad. Just DYING to be near you. But then i just ask myself a simple question. Would i rather have this or would i not be with you and its not really a question. I dont even have to think twice. I would choose this over not being with you. Without a doubt. We were not dealt an easy hand but fuck that. I love our relationship. Touch wood. Its the best thing that has happened to me in a long time. I always considered myself to be extremely unlucky. Now im starting to think otherwise. So what if we are in 2 different cities. So what if we see eachother after months. Its all so worth it. I dont regret it one bit. I dont regret anything. I feel we got pretty close even before our first date. Idk i feel most long distance relationships are extremely healthy ones. Cause loving someone and being with someone now to be with them in the future aint for the weak. 
+
+I love you and i miss you so much but i would choose this without a doubt. 🩷`
   },
   {
     day: 6,
